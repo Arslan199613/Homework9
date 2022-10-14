737303495fead6b6f7d5e0df3dbe090e22d678ed
Samsung <arslan.zakirov.1996@mail.ru>
2022-10-14 21:38:47 +0300

    Изменил названия переменных

--- src/Main.java
+++ src/Main.java
@@ -7,50 +7,48 @@ public class Main{
         }
         return arr;
     }
-
-    public static void main(String[] args) {
-        //Задача 1
-        System.out.println("Задание 1");
-        int[] total = generateRandomArray();
-        int sum = 0;
-        for (int j : total) {
-            sum += j;
-            System.out.println(j);
-        }
-        System.out.println();
-        System.out.println("Сумма трат за месяц составила  " + sum);
-        System.out.println();
-
-        //Задача 2
-        System.out.println("Задача 2");
-        int minimum = total[0];
-        for (int i = 0; i < total.length; i++) {
-            if (total[i] < minimum) {
-                minimum = total[i];
+        public static void main (String[]args){
+            //Задача 1
+            System.out.println("Задание 1");
+            int[] total = generateRandomArray();
+            int sum = 0;
+            for (int j : total) {
+                sum += j;
+                System.out.println(j);
             }
-        }
-        int maximum = total[0];
-        for (int i = 0; i < total.length; i++) {
-            if (total[i] > maximum) {
-                maximum = total[i];
+            System.out.println();
+            System.out.println("Сумма трат за месяц составила  " + sum);
+            System.out.println();
+
+            //Задача 2
+            System.out.println("Задача 2");
+            int mininumArrayElement = total[0];
+            for (int i = 0; i < total.length; i++) {
+                if (total[i] < mininumArrayElement) {
+                    mininumArrayElement = total[i];
+                }
             }
-        }
-        System.out.println("Минимальная сумма трат  " + minimum);
-        System.out.println("Максимальная сумма трат " + maximum);
-        System.out.println();
+            int maximumArrayElement = total[0];
+            for (int i = 0; i < total.length; i++) {
+                if (total[i] > maximumArrayElement) {
+                    maximumArrayElement = total[i];
+                }
+            }
+            System.out.println("Минимальная сумма трат  " + mininumArrayElement);
+            System.out.println("Максимальная сумма трат " + maximumArrayElement);
+            System.out.println();
 
-        //Задача 3
-        System.out.println("Задание 3");
-        int day = sum / 30;
-        System.out.println("Средняя сумма трат за месяц составила " + day + "рублей");
-        System.out.println();
+            //Задача 3
+            System.out.println("Задание 3");
+            int averageValueForTheMonth = sum / 30;
+            System.out.printf("Средняя сумма трат за месяц составила  " + averageValueForTheMonth + "рублей");
+            System.out.println();
 
-        //Задача 4
-        System.out.println("Задание 4");
-        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i = reverseFullName.length - 1; i >= 0; i--) {
-            System.out.print(reverseFullName[i]);
-
+            //Задача 4
+            System.out.println("Задание 4");
+            char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+            for (int i = reverseFullName.length - 1; i >= 0; i--) {
+                System.out.print(reverseFullName[i]);
+            }
         }
     }
-}
